718c09d4552be16fa2816ddf6011671e3b409602
eyupcelix7 <eyupturkey641@gmail.com>
2025-12-29 21:29:36 +0300

    Response sınıfı oluşturuldu.

---
+++ KuaforRandevuAPI.Common/Responses/ApiResponse.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KuaforRandevuAPI.Common.Responses
+{
+    public class ApiResponse<T>
+    {
+        public int StatusCode { get; set; }
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public T? Data { get; set; }
+        public List<string>? Errors { get; set; }
+        public static ApiResponse<T> SuccessResponse(T data, string? message = "")
+        {
+            return new ApiResponse<T>
+            {
+                StatusCode = 200,
+                Success = true,
+                Message = message,
+                Data = data
+            };
+        }
+        public static ApiResponse<T> ErrorResponse(string? message, List<string>? errors = null,int statusCode = 400)
+        {
+            return new ApiResponse<T>
+            {
+                StatusCode = statusCode,
+                Success = false,
+                Message = message,
+                Errors = errors
+            };
+        }
+    }
+}
