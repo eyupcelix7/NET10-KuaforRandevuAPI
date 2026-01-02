2fb59f2b2a9d2f4a40e644721c5e117bcad854f3
eyupcelix7 <eyupturkey641@gmail.com>
2026-01-02 22:25:51 +0300

    Id propertysi eklendi

---
+++ KuaforRandevuAPI.DataAccess/Migrations/20260102192442_addIdColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KuaforRandevuAPI.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class addIdColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "BarberServices",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "BarberServices");
+        }
+    }
+}
--- KuaforRandevuAPI.Entities/Concrete/BarberService.cs
+++ KuaforRandevuAPI.Entities/Concrete/BarberService.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using KuaforRandevuAPI.Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace KuaforRandevuAPI.Entities.Concrete
 {
-    public class BarberService
+    public class BarberService:IEntity
     {
+        public int Id { get; set; }
         public int BarberId { get; set; }
         public int ServiceId { get; set; }
         public bool IsActive { get; set; }
