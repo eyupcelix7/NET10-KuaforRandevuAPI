8b837409c4a83fc9fcc6ab9580d0c7bbf01f7ba4
eyupcelix7 <eyupturkey641@gmail.com>
2026-01-03 13:34:10 +0300

    FinancialTransactions tablosu hazırlandı.

---
+++ KuaforRandevuAPI.Entities/Concrete/FinancialTransactions.cs
@@ -0,0 +1,19 @@
+﻿using KuaforRandevuAPI.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KuaforRandevuAPI.Entities.Concrete
+{
+    public class FinancialTransactions:IEntity
+    {
+        // Gelir Gider işlemleri (Örn: Faturlar)
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Amount { get; set; }
+        public string? Description { get; set; }
+        public DateTime Date { get; set; }
+        public Barber? Barber { get; set; }
+        public int BarberId { get; set; }
+    }
+}
---
+++ KuaforRandevuAPI.Entities/Concrete/StockTransactions.cs
@@ -0,0 +1,19 @@
+﻿using KuaforRandevuAPI.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KuaforRandevuAPI.Entities.Concrete
+{
+    public class StockTransactions: IEntity
+    {
+        // Stok - Envanter işlemleri
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Quantity { get; set; }
+        public string? Description { get; set; }
+        public DateTime Date { get; set; }
+        public Barber? Barber { get; set; }
+        public int BarberId { get; set; }
+    }
+}
